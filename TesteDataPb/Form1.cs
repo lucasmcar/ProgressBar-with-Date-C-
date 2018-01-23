c71094087cd181b75b6fa4d4c4c68e4c583622d3
Lucas Martins de Carvalho <lmc.lucas90@gmail.com>
2018-01-23 13:51:05 -0200

    Update Form1.cs

--- TesteDataPb/Form1.cs
+++ TesteDataPb/Form1.cs
@@ -16,5 +16,28 @@ namespace TesteDataPb
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void tm_Tick(object sender, EventArgs e)
+        {
+            int dt = DateTime.Now.DayOfYear;
+
+
+            label1.Text = Convert.ToString(dt);
+
+            if (pbDia.Value <= dt)
+            {
+                pbDia.Value = pbDia.Value + dt;
+                label4.Text = "Hoje é o " + dt + "º dia do ano";
+            }
+            else
+            {
+                tm.Enabled = false;
+            }
+        }
     }
 }
